d650b8721dd157e95f41f125e90e0f6d430f36b5
Vinay Chabarwal <elvincentonino@gmail.com>
2025-07-03 10:28:27 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+# dataBase = mysql.connector.connect(
+#     host='localhost',
+#     user = 'elvin',
+#     password = 'Vinay@183784',
+
+#  )
+
+# cursorObject = dataBase.cursor()
+
+# cursorObject.execute(" CREATE DATABASE elvindb")
+
+# print("All done")
